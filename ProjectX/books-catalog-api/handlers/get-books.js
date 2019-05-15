532d59f5843e09c4054a3db4121145df09e3c769
AshKamdar <ash.tsys@gmail.com>
2019-05-15 09:02:27 +0000

    added handlerfor getbookbyid

--- ProjectX/books-catalog-api/handlers/get-books.js
+++ ProjectX/books-catalog-api/handlers/get-books.js
@@ -3,8 +3,19 @@
 const booklist = require('../handlers/booklist.json')
 
 
-function getBooks() {
+function getBooks(bookid) {
+
+  if (!bookid)
     return booklist;
+
+  const book = booklist.find((book) => {
+    return book.id == bookid
+  })
+
+  if (book)
+    return book
+
+  throw new Error('The book you requested was not found')
 }
 
 module.exports = getBooks
--- ProjectX/books-catalog-api/index.js
+++ ProjectX/books-catalog-api/index.js
@@ -6,11 +6,9 @@ booksApi = new ApiBuilder();
 const getBooks = require('./handlers/get-books')
 const addBook = require('./handlers/add-book')
 const deleteBook = require('./handlers/delete-book')
-
-//const updateBook = require('./handlers/update-book')
+const updateBook = require('./handlers/update-book')
 
 module.exports = booksApi;
-
 
 //default route
 booksApi.get('/', () => 'Welcome to Books Catalog API')
@@ -18,14 +16,41 @@ booksApi.get('/', () => 'Welcome to Books Catalog API')
 //GetBooks route
 booksApi.get('/books', () => {
   return getBooks()
+}, {
+  success: 201,
+  error: 400
+})
+
+//GetBookById route
+booksApi.get('/books/{id}', (request) => {
+  return getBooks(request.pathParams.id)
+}, {
+  error: 404
+}, {
+  success: 201,
+  error: 400
 })
 
 //AddBook route
 booksApi.post('/bookadd', (request) => {
   return addBook(request.body)
+}, {
+  success: 201,
+  error: 400
 })
 
 //DeleteBook route
 booksApi.delete('/bookdelete/{id}', (request) => {
   return deleteBook(request.pathParams.id)
+}, {
+  success: 201,
+  error: 400
+})
+
+//UpdateBook route
+booksApi.put('/bookupdate/{id}', (request) => {
+  return updateBook(request.pathParams.id)
+}, {
+  success: 201,
+  error: 400
 })
