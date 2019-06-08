81af00a6806418ed40ddf169521f4e077f3db868
AshKamdar <ash.tsys@gmail.com>
2019-06-08 12:25:21 +0000

    cleanup

--- Blogs/Blog13 - Deploy applications using Serverless framework/books-catalog-api/index.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
-
-app.get('/', function(req, res) {
-    res.send('Hello World!')
-})
-
-module.exports.handler = serverless(app);
